842a7dd25338c52f9f4a0ed2e9df86f5484fd6b5
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-26 22:20:00 +0600

    16. Create AppDbContext

---
+++ Microservice/Microservice.Services.CouponAPI/Data/CouponDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microservice.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservice.Services.CouponAPI.Data
+{
+    public class CouponDbContext : DbContext
+    {
+        public CouponDbContext(DbContextOptions<CouponDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
--- Microservice/Microservice.Services.CouponAPI/Models/Coupon.cs
+++ Microservice/Microservice.Services.CouponAPI/Models/Coupon.cs
@@ -1,10 +1,18 @@
-﻿namespace Microservice.Services.CouponAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Microservice.Services.CouponAPI.Models
 {
     public class Coupon
     {
+        [Key]
         public int CouponId { get; set; }
-        public string CouponCode { get; set; }
-        public double DiscountAmount { get; set; }
+
+        [Required]
+        public required string CouponCode { get; set; }
+
+        [Required]
+        public required double DiscountAmount { get; set; }
+
         public int MinAmount { get; set; }
     }
 }
