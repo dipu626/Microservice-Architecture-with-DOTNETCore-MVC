ca6cf6857cdf7c2eb3627e3d36b08df2eff7ba69
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-26 22:51:27 +0600

    19. Get all and get coupon by id

---
+++ Microservice/Microservice.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,56 @@
+﻿using Microservice.Services.CouponAPI.Data;
+using Microservice.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservice.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly CouponDbContext dbContext;
+
+        public CouponAPIController(CouponDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                List<Coupon> coupons = await dbContext.Coupons.ToListAsync();
+                return Ok(coupons);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return BadRequest();
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                Coupon? coupon = await dbContext.Coupons.FirstOrDefaultAsync(it => it.CouponId == id);
+
+                if (coupon == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(coupon);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return BadRequest();
+        }
+    }
+}
